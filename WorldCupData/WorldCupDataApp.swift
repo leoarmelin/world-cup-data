cb9b67957535c574493bb1f5b55a55f92a8221c8
leoarmelin <leonardo99.armelin@gmail.com>
2022-11-24 11:08:50 -0300

    Initial Commit

---
+++ WorldCupData/WorldCupDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCupDataApp.swift
+//  WorldCupData
+//
+//  Created by Leonardo Armelin on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldCupDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
